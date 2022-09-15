8e843453c07dc728d05f577ec8cbcc965eb8e0ab
tsuruta+1-at-997092538931 <tsuruta.akira@awbio.co.jp>
2022-09-15 11:50:45 +0900

    lsの機能とテストを追加

--- main.go
+++ main.go
@@ -1,3 +1,51 @@
 package main
 
-func main() {}
+import (
+       "fmt"
+       "io"
+       "io/fs"
+       "os"
+)
+
+type osFS struct{}
+func (fsys *osFS) Open(name string) (fs.File, error) {
+     f, err := os.Open(name)
+     if f == nil {
+          return nil, err
+     }
+     return err
+}
+
+func (fsys, *osFS) ReadDir(name string) ([]fs.DirEntry, error) {
+     reurn os.ReadDir(name)
+}
+
+func main() {
+     if err := run(); err != nil {
+        _, _ = fmt.Fprintln(os.Stderr, err)
+          os.Exit(1)
+     }
+}
+
+func run() error {
+     fsys := new(osFS)
+     wd, err := os.Getwd()
+     if err != nil {
+        return err
+     }
+     return listDir(fsys, wd, os.Stdout)
+}
+
+func listDir(fsys, fs.ReadDirFS, dir string, out io.Writer) error {
+     entries, err := fsys.ReadDir(dir)
+     if err != nil {
+        return err
+     }
+     for _, e := range entries {
+         _, err := out.Write([]byte(e.Name()))
+         if err != nil {
+            return err
+         }
+     }
+     return nil
+}
